b795ba0953570a5447cd597581627b3355b6ed93
kulenski <tsvetan@kulenski.eu>
2015-03-26 23:09:17 +0200

    ...

--- ImageRenamer/ImageItem.cs
+++ ImageRenamer/ImageItem.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace ImageRenamer
 {
+    // This class will replace MyFileItem
+    // and will have all the Get and Set methods
+    // and methods that returns file path, file name etc...
     class ImageItem
     {
 
